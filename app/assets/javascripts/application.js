b28250a7aac99d2790c40f969aa8baf320aba0ec
yurapanda <jerrome.pelacio@ama.edu.ph>
2016-08-02 17:07:18 +0800

    ddd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require bootstrap-sprockets
 //= require jquery
-//= require bootstrap.js
-
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-
 //= require_tree .
 
 
